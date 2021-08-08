51fbca23362b596859f453e62908dfdd0b3b2c46
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-08-08 22:27:42 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,47 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"cигнализация",
+        options:["alarm system", "fresh air", "crowded streets", "public transport"],
+        answer: 0
+    },
+    {
+        q:"cвежий воздух",
+        options:["feel isolated", "fresh air", "door chain", "beautiful landscapes"],
+        answer: 1
+    },
+    {
+        q:"интенсивное движение",
+        options:["alarm system", "crowded streets", "heavy traffic", "fresh air"],
+        answer: 2
+    },
+    {
+        q:"красивые пейзажи",
+        options:["heavy traffic", "fresh air", "crowded streets", "beautiful landscapes"],
+        answer: 3
+    },
+    {
+        q:"переполненные улицы",
+        options:["crowded streets", "door chain", "beautiful landscapes", "alarm system"],
+        answer: 0
+    },
+    {
+        q:"общественный транспорт",
+        options:["door chain", "public transport", "heavy traffic", "alarm system"],
+        answer: 1
+    },
+    {
+        q:"дверная цепь",
+        options:["heavy traffic", "feel isolated", "door chain", "alarm system"],
+        answer: 2
+    },
+    {
+        q:"чувствовать себя изолированным",
+        options:["fresh air", "beautiful landscapes", "crowded streets", "feel isolated"],
+        answer: 3
+    }
+
+
+
+]
